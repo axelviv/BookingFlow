1bba18b6b0dd67854288062148c9716b5971acaf
Foly <axelviv@live.com.ar>
2026-01-14 19:27:47 -0300

    Creacion de proyecto y SpaceService

---
+++ SpaceService/Program.cs
@@ -0,0 +1,22 @@
+// Program.cs base limpio
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
